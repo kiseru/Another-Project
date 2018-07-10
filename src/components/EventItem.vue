40b5258ac835ae1145a4e491a5e1375fbffb26d2
Alexandr Kiselev <kis.alex97@gmail.com>
2018-07-10 14:00:11 +0300

    Added link to event page

--- src/components/EventItem.vue
+++ src/components/EventItem.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="p-3 my-2 border border-dark rounded w-100">
-    <h4 class=" border-bottom border-dark p-0 m-0">Name</h4>
-    <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda consequatur consequuntur cumque deleniti dignissimos dolores ea eligendi impedit in iste itaque laboriosam modi nemo nobis, omnis perspiciatis quibusdam sed veritatis. Alias amet animi, consectetur deserunt doloribus eos est expedita nisi numquam, odit omnis perspiciatis provident quos voluptas voluptatem. Accusantium adipisci aspernatur deleniti dolorem molestias nostrum odio quia reiciendis soluta voluptate? Ab aperiam commodi, consectetur corporis cumque, dicta dolor, eaque error et eum expedita explicabo facilis fugiat incidunt laboriosam nesciunt nulla odit quasi quia reiciendis repellendus sapiente unde veniam veritatis voluptates. A ab accusamus accusantium adipisci alias, aliquam aliquid blanditiis corporis cupiditate debitis earum error et eveniet excepturi harum illum itaque magnam molestiae natus reprehenderit sapiente soluta sunt totam ullam voluptates.</p>
-    <div class="d-flex flex-row-reverse">
-      <p class="mb-0">Somebody Surname</p>
-    </div>
+  <div class="wrapper p-3 my-2 border border-dark rounded w-100">
+    <a href="/events/1" class="text-dark">
+      <h4 class="border-bottom border-dark p-0 m-0">Lorem ipsum dolor sit amet.</h4>
+      <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda consequatur consequuntur
+        cumque deleniti dignissimos dolores ea eligendi impedit in iste itaque laboriosam modi nemo nobis, omnis
+        perspiciatis quibusdam sed veritatis. Alias amet animi, consectetur deserunt doloribus eos est expedita nisi
+        numquam, odit omnis perspiciatis provident quos voluptas voluptatem. Accusantium adipisci aspernatur deleniti
+        dolorem molestias nostrum odio quia reiciendis soluta voluptate? Ab aperiam commodi, consectetur corporis
+        cumque, dicta dolor, eaque error et eum expedita explicabo facilis fugiat incidunt laboriosam nesciunt nulla
+        odit quasi quia reiciendis repellendus sapiente unde veniam veritatis voluptates.</p>
+      <div class="d-flex flex-row-reverse">
+        <p class="mb-0 font-weight-light font-italic">Author: Somebody Surname</p>
+      </div>
+    </a>
   </div>
 </template>
 
@@ -14,6 +22,8 @@
   }
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+  a:hover {
+    text-decoration: none;
+  }
 </style>
