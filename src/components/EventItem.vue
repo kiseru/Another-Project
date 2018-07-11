82e55fd760df4ba347561f3992468ad6f1a30464
Alexandr Kiselev <kis.alex97@gmail.com>
2018-07-11 14:51:35 +0300

    Added toggle button for switch between all and onley active events. Added getting data from server for events page.

--- src/components/EventItem.vue
+++ src/components/EventItem.vue
@@ -1,16 +1,10 @@
 <template>
   <div class="wrapper p-3 my-2 border border-dark rounded w-100">
-    <a href="/events/1" class="text-dark">
-      <h4 class="border-bottom border-dark p-0 m-0">Lorem ipsum dolor sit amet.</h4>
-      <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda consequatur consequuntur
-        cumque deleniti dignissimos dolores ea eligendi impedit in iste itaque laboriosam modi nemo nobis, omnis
-        perspiciatis quibusdam sed veritatis. Alias amet animi, consectetur deserunt doloribus eos est expedita nisi
-        numquam, odit omnis perspiciatis provident quos voluptas voluptatem. Accusantium adipisci aspernatur deleniti
-        dolorem molestias nostrum odio quia reiciendis soluta voluptate? Ab aperiam commodi, consectetur corporis
-        cumque, dicta dolor, eaque error et eum expedita explicabo facilis fugiat incidunt laboriosam nesciunt nulla
-        odit quasi quia reiciendis repellendus sapiente unde veniam veritatis voluptates.</p>
+    <a v-bind:href="`/events/${event.id}`" class="text-dark">
+      <h4 class="border-bottom border-dark p-0 m-0">{{event.name}}</h4>
+      <p class="mt-3">{{event.description}}</p>
       <div class="d-flex flex-row-reverse">
-        <p class="mb-0 font-weight-light font-italic">Author: Somebody Surname</p>
+        <p class="mb-0 font-weight-light font-italic">Author: {{event.owner.firstName}} {{event.owner.secondName}}</p>
       </div>
     </a>
   </div>
@@ -18,7 +12,13 @@
 
 <script>
   export default {
-    name: "EventItem"
+    name: "EventItem",
+    props: {
+      event: {
+        type: Object,
+        required: true
+      }
+    }
   }
 </script>
 
--- src/components/EventsComponent.vue
+++ src/components/EventsComponent.vue
@@ -3,6 +3,14 @@
     <div class="d-flex align-items-center justify-content-between p-3 px-4 my-3 text-white bg-dark rounded shadow-box">
       <div class="lh-100">
         <h4 class="mb-0 pb-0 text-white lh-100">Events</h4>
+        <div class="btn-group btn-group-toggle">
+          <label v-bind:class="['btn', 'btn-secondary', { active: isAll }]" v-on:click="allChosen">
+            <input type="radio" id="option1" autocomplete="off" v-bind:checked="isAll"/>All
+          </label>
+          <label v-bind:class="['btn', 'btn-secondary', { active: !isAll }]" v-on:click="activeChosen">
+            <input type="radio" id="option2" autocomplete="off" v-bind:checked="!isAll"/>Active
+          </label>
+        </div>
       </div>
       <div>
         <a href="/events/new" class="btn btn-light btn-sm">Create event</a>
@@ -10,17 +18,58 @@
     </div>
 
     <div class="align-items-center p-3 my-3 text-black bg-white rounded shadow-box">
-      <event-item/>
-      <event-item/>
+      <event-item v-for="eventItem in events" v-bind:event="eventItem"/>
     </div>
   </main>
 </template>
 
 <script>
   import EventItem from "./EventItem";
+  import axios from "axios";
+
   export default {
     name: "EventsComponent",
-    components: {EventItem}
+    components: {EventItem},
+    data() {
+      return {
+        isAll: true,
+        events: [
+          {
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda consequatur consequuntur\n" +
+            "        cumque deleniti dignissimos dolores ea eligendi impedit in iste itaque laboriosam modi nemo nobis, omnis\n" +
+            "        perspiciatis quibusdam sed veritatis. Alias amet animi, consectetur deserunt doloribus eos est expedita nisi\n" +
+            "        numquam, odit omnis perspiciatis provident quos voluptas voluptatem. Accusantium adipisci aspernatur deleniti\n" +
+            "        dolorem molestias nostrum odio quia reiciendis soluta voluptate? Ab aperiam commodi, consectetur corporis\n" +
+            "        cumque, dicta dolor, eaque error et eum expedita explicabo facilis fugiat incidunt laboriosam nesciunt nulla\n" +
+            "        odit quasi quia reiciendis repellendus sapiente unde veniam veritatis voluptates.",
+            id: 0,
+            name: "Lorem ipsum dolor sit amet.",
+            owner: {
+              birthDate: "2018-07-11T10:55:18.882Z",
+              email: "string",
+              firstName: "Somebody",
+              id: 0,
+              middleName: "string",
+              role: "string",
+              secondName: "Sirname"
+            },
+            siteUrl: "string"
+          }
+        ]
+      }
+    },
+    methods: {
+      allChosen() {
+        this.isAll = true;
+        axios.get("/users/events")
+          .then(response => this.events = response.data);
+      },
+      activeChosen() {
+        this.isAll = false;
+        axios.get("/users/events/active")
+          .then(response => this.events = response.data);
+      }
+    }
   }
 </script>
 
