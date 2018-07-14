9447b1903f30694296b840d93b802ebfbfad2baf
Alexandr Kiselev <kis.alex97@gmail.com>
2018-07-14 12:29:26 +0300

    Fix displaying pattern field required settings component.

--- src/components/PatternFieldRequiredSettingComponent.vue
+++ src/components/PatternFieldRequiredSettingComponent.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="p-1">
-    <label>
-      Required:
-      <input type="radio" v-model="patternElement.required" :value="true"> Yes
-      <input type="radio" v-model="patternElement.required" :value="false"> No
-    </label>
+  <div class="d-flex p-1">
+    <!--<label>-->
+    <p class="mb-0 pr-2">Required:</p>
+    <div class="form-check pr-2">
+      <input type="radio" class="form-check-input" v-model="patternElement.required" :value="true">
+      <label class="form-check-label">Yes</label>
+    </div>
+
+    <div class="form-check pr-2">
+      <input type="radio" class="form-check-input" v-model="patternElement.required" :value="false">
+      <label class="form-check-label">No</label>
+    </div>
+    <!--</label>-->
   </div>
 </template>
 <script>
