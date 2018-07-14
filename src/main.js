2dbd7879fab0ab71afbf6f45a628cf8bf3acbc2b
Alexandr Kiselev <kis.alex97@gmail.com>
2018-07-14 20:57:24 +0300

    Added vuex

--- src/main.js
+++ src/main.js
@@ -1,19 +1,26 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import VueRouter from 'vue-router';
 import App from './App.vue';
 
 import routes from './routes';
 
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 const router = new VueRouter({
   mode: 'history',
   routes
 });
 
+const store = new Vuex.Store({
+
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount('#app');
