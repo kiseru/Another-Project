7426c2c58491ba15ee59c97edc09f274285436c6
Alexandr Kiselev <kis.alex97@gmail.com>
2018-07-14 12:39:26 +0300

    Fixed displaying text component.

--- src/components/PatternTextComponent.vue
+++ src/components/PatternTextComponent.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="m-2">
-    <label>{{patternElement.name}}<span class="text-danger" v-if="patternElement.required">*</span>
-      <input class="ml-2" v-model="selected" type="text" placeholder="Type something">
-    </label>
+  <div class="form-group m-2">
+    <label>{{patternElement.name}}<span class="text-danger" v-if="patternElement.required">*</span></label>
+    <input class="form-control ml-2" v-model="selected" type="text" placeholder="Type something">
   </div>
 </template>
 <script>
@@ -21,16 +20,3 @@
     }
   }
 </script>
-<style lang="less" scoped>
-  input {
-    border-radius: 5px;
-    height: 40px;
-    border: 1px solid #6c757d;
-    padding: 8px;
-    font-size: 16px;
-
-    &:focus {
-      outline: none;
-    }
-  }
-</style>
