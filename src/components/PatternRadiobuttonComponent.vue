8a936b17c8dd7d3f63c4970c92a0ac2e95460327
Alexandr Kiselev <kis.alex97@gmail.com>
2018-07-14 12:35:19 +0300

    Fixed displaying radio buttons component.

--- src/components/PatternRadiobuttonComponent.vue
+++ src/components/PatternRadiobuttonComponent.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="m-2">
+  <div class="form-group m-2">
     <p class="mb-1">{{patternElement.name}}<span class="text-danger" v-if="patternElement.required">*</span></p>
-    <span class="ml-2" v-for="value in patternElement.selectableValue">
-      <input type="radio" v-model="selected" :name="patternElement.id" :value="value"><span class="ml-2 mb-1">{{value}}</span><br>
-    </span>
+    <div class="form-check ml-2" v-for="value in patternElement.selectableValue">
+      <input class="form-check-input" type="radio" v-model="selected" :name="patternElement.id" :value="value">
+      <label class="form-check-label ml-2 mb-1">{{value}}</label>
+    </div>
   </div>
 </template>
 <script>
