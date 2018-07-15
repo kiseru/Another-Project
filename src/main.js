350b0b8e46226faaf6a3d14222be26ace0e7bef6
Alexandr Kiselev <kis.alex97@gmail.com>
2018-07-15 15:00:22 +0300

    Putted vue-router in a separate file.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,7 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import App from './App.vue';
-
-import routes from './routes';
 import store from './store';
-
-Vue.use(VueRouter);
-Vue.use(Vuex);
-
-const router = new VueRouter({
-  mode: 'history',
-  routes
-});
-
-Vue.config.productionTip = false;
+import router from './router';
 
 new Vue({
   render: h => h(App),
---
+++ src/router/routes.js
@@ -0,0 +1,11 @@
+import CreatePatternComponent from '../components/CreatePatternComponent';
+import EventComponent from '../components/EventComponent';
+import EventsComponent from '../components/EventsComponent';
+import PatternComponent from '../components/PatternComponent';
+
+export default [
+  {path: '/events/:id/pattern/new', component: CreatePatternComponent},
+  {path: '/events/:id/pattern', component: PatternComponent},
+  {path: '/events/:id', component: EventComponent},
+  {path: '/events', component: EventsComponent}
+];
