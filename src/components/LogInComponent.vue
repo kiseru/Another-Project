00d501eff9f607b6326dbb3a7aafa3aeda74cb23
Alexandr Kiselev <kis.alex97@gmail.com>
2018-07-20 15:46:52 +0300

    Added login page without redirection

---
+++ src/components/LogInComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <main role="main" class="container pt-5">
+    <div class="bg-white rounded p-3 shadow mx-auto text-center" style="width: 25rem">
+      <h3 class="border-bottom">Войти</h3>
+      <div class="mt-3">
+        <div class="form-group">
+          <label for="loginInput">E-mail</label>
+          <input class="form-control"
+                 id="loginInput"
+                 type="text"
+                 v-model="user.email">
+        </div>
+
+        <div class="form-group">
+
+          <label for="passwordInput">Пароль</label>
+          <input class="form-control"
+                 id="passwordInput"
+                 type="password"
+                 v-model="user.password">
+        </div>
+      </div>
+
+      <div class="pt-2">
+        <button class="btn btn-primary w-100"
+                @click="logIn">Войти</button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+  import axios from "axios";
+
+  export default {
+    name: "LogInComponent",
+    data() {
+      return {
+        user: {
+          email: "",
+          password: ""
+        }
+      }
+    },
+    methods: {
+      logIn() {
+        axios.post(`${this.$store.state.globalUrl}/users/login`);
+      }
+    }
+  }
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,7 @@ import CreatingEventComponent from '../components/CreatingEventComponent';
 import CreatePatternComponent from '../components/CreatePatternComponent';
 import EventComponent from '../components/EventComponent';
 import EventsComponent from '../components/EventsComponent';
+import LogInComponent from '../components/LogInComponent';
 import PatternComponent from '../components/PatternComponent';
 
 export default [
@@ -9,5 +10,7 @@ export default [
   {path: '/events/:id', component: EventComponent},
   {path: '/events', component: EventsComponent},
   {path: '/organizers/events/new', component: CreatingEventComponent},
-  {path: '/organizers/events/:id/pattern/new', component: CreatePatternComponent}
+  {path: '/organizers/events/:id/pattern/new', component: CreatePatternComponent},
+  {path: '/login', component: LogInComponent},
+  {path: '/', component: LogInComponent}
 ];
