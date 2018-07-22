87e46aa35081d6f1bd1ac0444fe103087d51ba91
kiseru <kis.alex97@gmail.com>
2018-07-22 21:00:08 +0300

    Added validation to login form.

--- src/components/LogInComponent.vue
+++ src/components/LogInComponent.vue
@@ -9,6 +9,9 @@
                  id="loginInput"
                  type="text"
                  v-model="user.email">
+          <div class="alert alert-danger mt-2"
+               v-if="!validations.emailValid">Неверно введен e-mail
+          </div>
         </div>
 
         <div class="form-group">
@@ -18,12 +21,16 @@
                  id="passwordInput"
                  type="password"
                  v-model="user.password">
+          <div class="alert alert-danger mt-2"
+               v-if="!validations.passwordValid">Пароль должен содержать больше 6 и меньше 40 символов
+          </div>
         </div>
       </div>
 
       <div class="pt-2">
         <button class="btn btn-primary w-100"
-                @click="logIn">Войти</button>
+                @click="logIn">Войти
+        </button>
       </div>
     </div>
   </main>
@@ -39,13 +46,26 @@
         user: {
           email: "",
           password: ""
+        },
+        validations: {
+          emailValid: true,
+          passwordValid: true
         }
       }
     },
     methods: {
       logIn() {
-        axios.post(`${this.$store.state.globalUrl}/users/login`);
+        axios.post(`${this.$store.state.globalUrl}/users/login`, user);
       }
+    },
+    watch: {
+      "user.email"(newEmail, oldEmail) {
+        const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/i;
+        this.validations.emailValid = newEmail.search(regex) !== -1;
+      },
+      "user.password"(newPassword, oldPassword) {
+        this.validations.passwordValid = newPassword.length >= 6 && newPassword.length <= 40;
+      },
     }
   }
 </script>
